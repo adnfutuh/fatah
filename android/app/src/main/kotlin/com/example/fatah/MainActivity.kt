1d233bc813be45d0614b73a57273c91ba15a6b13
rizki <rizkiadnanfutuh@gmail.com>
2024-07-19 13:31:26 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fatah/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fatah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Fatah extends StatelessWidget {
+  const Fatah({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // String introduceName = 'my name is ';
+    // String introduceAge = 'mow, my name is';
+    //String introduceCountryOrigin = 'i\'m from';//
+    return const SafeArea(
+      child: Column(
+        children: [
+          Text('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'),
+          SizedBox(
+            height: 10,
+          ),
+          Text('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'),
+        ],
+      ),
+    );
+  }
+}
