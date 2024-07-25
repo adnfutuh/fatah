d795e789d2ab99ddd245fd0cab86542ec450a881
rizki <rizkiadnanfutuh@gmail.com>
2024-07-25 12:49:46 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,50 @@
-List<List<String>> restoA = [
-  [nasi ['nasi goreng',
-    'nasi liwet',
-    'nasi pecak',
-    'nasi daun jeruk',
-    'nasi kuning',
-    'nasi uduk']
-    
-  ],
-  [
-    'Sayur Lodeh',
-    'Sayur Asam',
-    'Capcay',
-    'Sayur nangka',
-    'Sayur pokcoy',
-    'sayur bayam',
-    'sayur kangkung'
-  ],
-  ['Bakwan', ' tempe', 'tahu isi', 'mendoan', 'tahu bulat']
+List<String> nasi = [
+  'nasi goreng',
+  'nasi liwet',
+  'nasi pecak',
+  'nasi daun jeruk',
+  'nasi kuning',
+  'nasi uduk'
+];
+List<String> sayur = [
+  'Sayur Lodeh',
+  'Sayur Asam',
+  'Capcay',
+  'Sayur nangka',
+  'Sayur pokcoy',
+  'sayur bayam',
+  'sayur kangkung'
+];
+List<String> gorengGorengan = [
+  'Bakwan',
+  ' tempe',
+  'tahu isi',
+  'mendoan',
+  'tahu bulat',
 ];
 
+List<List<dynamic>> restoA = [
+  [List.from(nasi)],
+  [List.from(sayur)],
+  [List.from(gorengGorengan)],
+  [List.from(minuman)],
+];
+
+List<String> minuman = [
+  'Fanta',
+  'sprite',
+  'Coca-Cola',
+  'susu',
+  'teh',
+  'lemon',
+  'sisri',
+  'sosro'
+];
+List<String> dataUntukDihapus = List.from(restoA);
 
 void main() {
+  dataUntukDihapus.remove('nasi uduk');
+  dataUntukDihapus.remove('sayur asam');
+  dataUntukDihapus.remove('mendoan');
   print(restoA);
 }
