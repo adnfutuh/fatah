8907a471a668971962877cc410c02cd923d5484c
rizki <rizkiadnanfutuh@gmail.com>
2024-07-29 12:01:03 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,8 @@
-List<String> nasi = [
-  'nasi goreng',
-  'nasi liwet',
-  'nasi pecak',
-  'nasi daun jeruk',
-  'nasi kuning',
-  'nasi uduk'
-];
-List<String> sayur = [
-  'Sayur Lodeh',
-  'Sayur Asam',
-  'Capcay',
-  'Sayur nangka',
-  'Sayur pokcoy',
-  'sayur bayam',
-  'sayur kangkung'
-];
-List<String> gorengGorengan = [
-  'Bakwan',
-  ' tempe',
-  'tahu isi',
-  'mendoan',
-  'tahu bulat',
-];
-List<String> minuman = [
-  'Fanta',
-  'sprite',
-  'Coca-Cola',
-  'susu',
-  'teh',
-  'lemon',
-  'sisri',
-  'sosro'
-];
-
-
-List<List<String>> restoA = [];
-restoA.add(nasi);
-
-
- 
-List<List<String>> dataUntukDihapus = List.from(restoA);
-
 void main() {
-  dataUntukDihapus[0].remove('nasi uduk');
-  dataUntukDihapus[1].remove('sayur bayam');
-  dataUntukDihapus[2].remove('mendoan');
-  print(dataUntukDihapus);
+  List<String> namaSiswa = ['fatIHIN', 'RizKI', 'fAtAh'];
+  namaSiswa = namaSiswa.map((namaSiswa) => namaSiswa.toLowerCase()).toList();
+
+  int panjangListNamaSiswa = namaSiswa.length; // nilainya 3
+  for (int index = 0; index < panjangListNamaSiswa; index++) {}
+  print(namaSiswa);
 }
