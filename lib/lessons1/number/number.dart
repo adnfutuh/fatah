1cdd8f56bb5ebc29843f3859dfdfde739950d177
rizki <rizkiadnanfutuh@gmail.com>
2024-07-30 13:09:31 +0700

    number

---
+++ lib/lessons1/number/number.dart
@@ -0,0 +1,15 @@
+// NUM NUMBER = bisa INT dan DOUBLE
+
+//INT = Bilangan bulat
+//double = Bilangan desimal
+
+void main() {
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+  num number = 10;
+  number = 10.5;
+  print(number);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 void main() {
   List<String> namaSiswa = ['fatIHIN', 'RizKI', 'fAtAh'];
   namaSiswa = namaSiswa.map((namaSiswa) => namaSiswa.toLowerCase()).toList();
+  namaSiswa = namaSiswa.map((namaSiswa) => namaSiswa.toUpperCase()).toList();
 
   int panjangListNamaSiswa = namaSiswa.length; // nilainya 3
   for (int index = 0; index < panjangListNamaSiswa; index++) {}
