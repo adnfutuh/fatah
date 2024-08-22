97714aae16cf130102db6c3e67b9fb725b85d0c5
rizki <rizkiadnanfutuh@gmail.com>
2024-08-22 23:20:13 +0700

    up

--- lib/lessons1/null_safety/null_safety.dart
+++ lib/lessons1/null_safety/null_safety.dart
@@ -6,8 +6,17 @@ void main() {
   String? name;
   name = 'eko';
 
+  String? nama;
+  String siswa = nama ?? 'adnan';
+  print(siswa);
+
+  String? mahaSiswa;
+  String dosen = mahaSiswa!; //konversi seacara paksa,jika di run null maka eror
+
   int? nullablePrice = null;
-  if (nullablePrice != null) {
-    int price = nullablePrice;
+  double? price = nullablePrice?.toDouble(); //jadi nullable
+  int? amount = null;
+  if (amount != null) {
+    double amounts = amount.toDouble();
   }
 }
