c35d2d2877701bd083b0e68550e091484ae5d31f
rizki <rizkiadnanfutuh@gmail.com>
2024-08-22 23:20:07 +0700

    up

---
+++ lib/lessons1/function/function2/function_short_expression.dart
@@ -0,0 +1,10 @@
+int sum(int first, int second) => first + second; //di singkat
+// {
+//   return first + second;
+// }
+
+void main() {
+  var total = sum(100, 10);
+  print(total);
+  print(sum(2, 2));
+}
