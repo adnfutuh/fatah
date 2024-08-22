41113832f4e0c2e283ca2240d4a0f5442186f261
rizki <rizkiadnanfutuh@gmail.com>
2024-08-22 23:20:22 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
-// void main() {
-//   int? age = null;
-//   if (age != null) {
-//     print(age.toDouble());
-//   }
-//   String? name;
-//   name = 'eko';
-
-//   int? nullablePrice = null;
-//   if (nullablePrice != null) {
-//     int price = nullablePrice;
-//   }
+int sum(int first, int second) => first + second; //di singkat
+// {
+//   return first + second;
 // }
+
 void main() {
-  var nilai = null;
-  // var ucapan = nilai >= 75 ? 'anda lulus' : 'coba lagi';
-  var ucapan = nilai ?? 'agus';
-  print(ucapan);
+  var total = sum(100, 10);
+  print(total);
 }
