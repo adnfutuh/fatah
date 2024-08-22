a87352e95252e9858312ac0c468022f6af592d9b
rizki <rizkiadnanfutuh@gmail.com>
2024-08-22 23:27:30 +0700

    up

--- lib/lessons1/function/function2/inner_function.dart
+++ lib/lessons1/function/function2/inner_function.dart
@@ -5,8 +5,11 @@ void contoh() {
 void main() {
   void contoh1() {
     print('aku');
+    void contoh2() {}
+    contoh2();
   }
 
   contoh1();
+  // contoh2(); //eror
 }
 //inner function hanya bisa di akses didalam outer function
