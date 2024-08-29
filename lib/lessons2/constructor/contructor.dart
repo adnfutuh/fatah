73349452a219a8f7ba743b082884631d254e34b2
rizki <rizkiadnanfutuh@gmail.com>
2024-08-29 22:48:32 +0700

    up

--- lib/lessons2/constructor/contructor.dart
+++ lib/lessons2/constructor/contructor.dart
@@ -2,9 +2,9 @@ class Person {
   String name = 'guest';
   String? adress;
   final String country = 'indonesia';
-  Person(String paramName, String address) {
+  Person(String paramName, String adress) {
     //name=paramname
-    adress = address;
+    this.adress = adress; //this mengakses object saat ini
     paramName = name;
   }
 }
