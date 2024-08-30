ff850920389f611cc85c9b86072942a74a4bcc52
rizki <rizkiadnanfutuh@gmail.com>
2024-08-30 18:22:28 +0700

    up

--- lib/lessons2/casecade notation/cascade_notation.dart
+++ lib/lessons2/casecade notation/cascade_notation.dart
@@ -1,13 +1,20 @@
-//tanpa casecade notation
 class User {
   String? username;
   String? name;
   String? email;
 }
 
+User? createUser() {
+  return null;
+}
+
 void main() {
   var user = User()
     ..username = 'aoshi'
+    ..name = 'mukidi sanjaya'
+    ..email = 'sanjaya.gmail';
+  User? user2 = createUser()
+    ?..username = 'aoshi'
     ..name = 'mukidi sanjaya'
     ..email = 'sanjaya.gmail';
 }
