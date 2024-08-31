5b759cdc7e62fac48515616e5e1b04832bd0dc75
rizki <rizkiadnanfutuh@gmail.com>
2024-08-31 22:43:31 +0700

    up

---
+++ lib/OOP/abstract/method_abstract.dart
@@ -0,0 +1,16 @@
+abstract class Animal {
+  String? name;
+  void run();
+}
+
+class Cat extends Animal {
+  void run() {
+    print('hello $name');
+  }
+}
+
+void main() {
+  var cat = Cat();
+  cat.name = 'ciko';
+  cat.run();
+}
