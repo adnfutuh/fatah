e1b08355320ed1d8bacafa4f0d0a295ba742b30d
rizki <rizkiadnanfutuh@gmail.com>
2024-08-31 16:57:34 +0700

    Up

---
+++ lib/lessons2/inheritance/inheritance.dart
@@ -0,0 +1,23 @@
+class Manager {
+  String? name;
+  void sayhello(String name) {
+    print('hello $name, my name is ${this.name}');
+  }
+}
+
+class Boss extends Manager {
+  void hello() {
+    print('hello $name,my name is ${this.name}');
+  }
+}
+
+void main() {
+  var manager = Manager();
+  manager.name = 'joko';
+  manager.sayhello('agus');
+
+  var smanager = Boss();
+  smanager.name = 'joko';
+  smanager.sayhello('agus');
+  smanager.hello();
+}
