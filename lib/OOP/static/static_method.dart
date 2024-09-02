2bd639e6cd8f0127692c6cfd06e67552b8ee8f04
rizki <rizkiadnanfutuh@gmail.com>
2024-09-03 04:09:10 +0700

    up

---
+++ lib/OOP/static/static_method.dart
@@ -0,0 +1,8 @@
+//biasa digunakan di method yang utilityseperti herper herper(embatntu)
+class Math {
+  static int sum(int first, int second) => first + second;
+}
+
+void main() {
+  print(Math.sum(10, 10));
+}
