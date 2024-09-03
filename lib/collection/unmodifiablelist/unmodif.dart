abee91a802148c406833b0c46f9275fb960d0738
rizki <rizkiadnanfutuh@gmail.com>
2024-09-03 20:16:09 +0700

    up

---
+++ lib/collection/unmodifiablelist/unmodif.dart
@@ -0,0 +1,16 @@
+//tidak bisa dimodifikasi baik fix ataupun grow
+
+import 'dart:collection';
+
+void main() {
+  var list = List<int>.filled(10, 0);
+  print(list);
+  list[0] = 1;
+  list[1] = 2;
+  list[2] = 3;
+  list[3] = 4;
+  list[4] = 5;
+  print(list);
+  var unmodif = UnmodifiableListView(list);
+  print(unmodif);
+}
