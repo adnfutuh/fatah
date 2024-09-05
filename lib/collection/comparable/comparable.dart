dae0adacac079f6aca05372ca12ed8f455a61f67
rizki <rizkiadnanfutuh@gmail.com>
2024-09-05 16:58:22 +0700

    up

---
+++ lib/collection/comparable/comparable.dart
@@ -0,0 +1,27 @@
+import 'dart:collection';
+
+class Category implements Comparable<Category> {
+  String id;
+  String name;
+  Category(this.id, this.name);
+  @override
+  int compareTo(Category other) {
+    return id.compareTo(other.id);
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$id,$name';
+  }
+}
+
+void main() {
+  final treeset = SplayTreeSet();
+  treeset.add(Category('z', 'category1'));
+  treeset.add(Category('3', 'category3'));
+  treeset.add(Category('1', 'category1'));
+  treeset.add(Category('j', 'category8'));
+
+  print(treeset);
+}
