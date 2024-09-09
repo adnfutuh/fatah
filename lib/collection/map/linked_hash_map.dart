441a36b49d4f95128d5e3c68d025acdfebe00d80
rizki <rizkiadnanfutuh@gmail.com>
2024-09-09 21:26:22 +0700

    up

---
+++ lib/collection/map/linked_hash_map.dart
@@ -0,0 +1,15 @@
+import 'dart:collection';
+
+void main() {
+  // final scores = Map<String, int>();
+  final scores = LinkedHashMap<String, int>();
+  scores['eko'] = 100;
+  scores['budi'] = 100;
+  scores['agus'] = 100;
+  scores['sajaya'] = 100;
+  scores['sutomo'] = 100;
+  scores['ekaury'] = 100;
+  scores['syauqi'] = 100;
+  scores['anung'] = 100;
+  print(scores);
+}
