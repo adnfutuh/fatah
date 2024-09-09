c222fd71ec7cb3eaf25cf4b1b534fa477ccc67e6
rizki <rizkiadnanfutuh@gmail.com>
2024-09-09 21:26:15 +0700

    up

---
+++ lib/collection/map/hash_map.dart
@@ -0,0 +1,14 @@
+import 'dart:collection';
+
+void main() {
+  final scores = HashMap<String, int>();
+  scores['eko'] = 100;
+  scores['budi'] = 100;
+  scores['agus'] = 100;
+  scores['sajaya'] = 100;
+  scores['sutomo'] = 100;
+  scores['ekaury'] = 100;
+  scores['syauqi'] = 100;
+  scores['anung'] = 100;
+  print(scores);
+}
