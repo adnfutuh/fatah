875ca687ad819c8955b750c2033a5207b0bf9953
rizki <rizkiadnanfutuh@gmail.com>
2024-09-11 05:52:38 +0700

    up

--- lib/collection/linked_hash_set/linked_hash_set.dart
+++ lib/collection/linked_hash_set/linked_hash_set.dart
@@ -1,11 +1,20 @@
+// import 'dart:collection';
+
+// void main() {
+//   // final set=<String>{};
+//   final set = LinkedHashSet();
+//   set
+
+//     ..add('rizki')
+//     ..add('adnan')
+//     ..add('futuh');
+//   print(set);
+// }
 import 'dart:collection';
 
 void main() {
-  // final set=<String>{};
-  final set = LinkedHashSet();
-  set
-    ..add('rizki')
-    ..add('adnan')
-    ..add('futuh');
-  print(set);
+  LinkedHashSet<String> contoh = LinkedHashSet();
+  contoh.add('aple');
+  contoh.add('pisang');
+  print(contoh);
 }
