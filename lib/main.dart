8ffd7a8af2798d2091bf9f73fca0980cfc09ec80
rizki <rizkiadnanfutuh@gmail.com>
2024-09-22 07:14:49 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
-Future<void> main() async {
-  print('Fetching username...');
-  var username = await fetchUsername();
-  print('You are logged in as $username');
-  print('Welcome!');
+Future<String> firstName() async {
+  return "Rizki";
 }
 
-Future<String> fetchUsername() {
-  return Future.delayed(Duration(seconds: 3), () => 'DartUser');
+Future<String> middleName() async {
+  return "Adnan";
+}
+
+Future<String> lastName() async {
+  return "Futuh";
+}
+
+Future<String> sayHello(String name) async {
+  return "Hello $name";
+}
+
+void main() {
+  firstName().then((firstName) {
+    return middleName().then((middleName) {
+      return lastName().then((lastName) {
+        var fullName = "$firstName $middleName $lastName";
+        return sayHello(fullName);
+      });
+    });
+  }).then((response) => print(response));
+  print("done");
 }
