264ecd2539398cafa9489e3a90edcd839ef919f0
rizki <rizkiadnanfutuh@gmail.com>
2024-09-24 02:51:58 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,1 @@
-Future<void> hello() async {
-  await Future.delayed(Duration(seconds: 2), () {
-    print('progammer zaman now');
-  });
-}
 
-void main() async {
-  await hello();
-  print('done');
-}
