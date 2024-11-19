af2cc7d9be4880322e974d6b3ef39de97a744236
rizki <rizkiadnanfutuh@gmail.com>
2024-11-19 23:20:52 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
-//fitur untuk membuat data yang sudah jelas seperti jenis kelamin
-enum CustomerLevel {
-  regular(title: 'Regular'),
-  premium(title: 'Premium'),
-  vip(title: 'Vip'),
-  raja(title: 'Raja'),
-  ;
+// //fitur untuk membuat data yang sudah jelas seperti jenis kelamin
+// enum CustomerLevel {
+//   regular(title: 'Regular'),
+//   premium(title: 'Premium'),
+//   vip(title: 'Vip'),
+//   raja(title: 'Raja'),
+//   ;
 
-  final String title;
-  const CustomerLevel({required this.title});
-}
+//   final String title;
+//   const CustomerLevel({required this.title});
+// }
 
-class Customer {
-  String name;
-  CustomerLevel level;
-  Customer(this.name, this.level);
-}
+// class Customer {
+//   String name;
+//   CustomerLevel level;
+//   Customer(this.name, this.level);
+// }
 
-void main() {
-  // var customer = Customer('adnan', CustomerLevel.vip);
-  print(CustomerLevel);
-  // print(customer.level);
-  // print(customer.level.name);
-  // print(customer.level.title);
-  print(CustomerLevel.values); //values=mengambil semua data menggunakan list
-  print(CustomerLevel.vip); //values=mengambil semua data menggunakan list
-  print(CustomerLevel
-      .regular.name); //values=mengambil semua data menggunakan list
-  print(CustomerLevel
-      .regular.title); //values=mengambil semua data menggunakan list
-}
+// void main() {
+//   // var customer = Customer('adnan', CustomerLevel.vip);
+//   print(CustomerLevel);
+//   // print(customer.level);
+//   // print(customer.level.name);
+//   // print(customer.level.title);
+//   print(CustomerLevel.values); //values=mengambil semua data menggunakan list
+//   print(CustomerLevel.vip); //values=mengambil semua data menggunakan list
+//   print(CustomerLevel
+//       .regular.name); //values=mengambil semua data menggunakan list
+//   print(CustomerLevel
+//       .regular.title); //values=mengambil semua data menggunakan list
+// }
